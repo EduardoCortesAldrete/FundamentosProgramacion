c53056f2597d3cf73aa7261880f6829b0a1bcff5
Eduardo Cortes <>
2022-08-25 17:39:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Hola-Mundo/main.c
@@ -0,0 +1,15 @@
+/*Hola mundo
+//Eduardo Cortes
+//25-08-2022
+version 1 */
+
+//Bibliotecas es igual al lenguaje de alguien mas 
+#include <stdio.h>
+
+//Funcion principal
+int main(void) {
+
+  //Funcion de impresion en pantalla
+  printf("Hola mundo\n");
+  return 0;
+}
